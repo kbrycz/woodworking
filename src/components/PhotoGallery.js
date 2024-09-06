75a0c70a73924d875c42609cdaae65a29b3a2f35
Karl Brycz <kbrycz@umich.edu>
2024-09-05 23:07:04 -0500

    mobile

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import './PhotoGallery.css'; // Gallery styles
 import PhotoModal from './PhotoModal'; // Import the modal component
-import logo512 from '../images/logo512.png'; // Sample image for the gallery
+import chess from '../images/chess.png'; // Sample image for the gallery
+import cutting from '../images/cutting.png'; // Sample image for the gallery
+import desk from '../images/desk.png'; // Sample image for the gallery
+import end_table from '../images/end_table.png'; // Sample image for the gallery
+
 
 const photos = [
-  { id: 1, title: "Photo 1", src: logo512, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { id: 2, title: "Photo 2", src: logo512, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { id: 3, title: "Photo 3", src: logo512, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { id: 4, title: "Photo 4", src: logo512, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { id: 5, title: "Photo 5", src: logo512, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { id: 6, title: "Photo 6", src: logo512, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+  { id: 1, title: "Chessboard", src: chess, description: "A simple chessboard created with cherry and walnut." },
+  { id: 2, title: "Cutting board", src: cutting, description: "A cutting board created with walnut and cherry." },
+  { id: 3, title: "Desk", src: desk, description: "A cherry desk top with standing desk legs." },
+  { id: 4, title: "End table", src: end_table, description: "A poplar endtable made with nice jointery." }
 ];
 
 const PhotoGallery = () => {
